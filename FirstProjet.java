ccb3d8de7d5aee176aab277cde9f8364f3fa877f
Karthiattri <65332923+Karthiattri@users.noreply.github.com>
2020-05-18 14:44:36 +0530

    Update FirstProjet.java

--- FirstProjet.java
+++ FirstProjet.java
@@ -4,10 +4,12 @@ public class FirstProjet{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("SIA BABA");
-		System.out.println("OHM Shree Sai Namaonamga");
+		System.out.println("Ohm Shree Sai Namaonamga");
 		System.out.println("Shree Sai Namaonamga");
 		System.out.println("Jai jai Sai Namaonamga");
 		System.out.println("Sathguru Sai Namaonamga");
+		System.out.println("Mis u Sai Namaonamga");
+		
 	}
 
 }
