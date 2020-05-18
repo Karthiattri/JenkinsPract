e09acd4733c3b25bdcf1b339f518a523068b7bf0
Karthiattri <65332923+Karthiattri@users.noreply.github.com>
2020-05-18 13:41:37 +0530

    Jenkinsprojt

---
+++ FirstProjet.java
@@ -0,0 +1,13 @@
+
+public class FirstProjet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SIA BABA");
+		System.out.println("OHM Shree Sai Namaonamga");
+		System.out.println("Shree Sai Namaonamga");
+		System.out.println("Jai jai Sai Namaonamga");
+		System.out.println("Sathguru Sai Namaonamga");
+	}
+
+}
